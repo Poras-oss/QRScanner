dc9bacc0bf43d66456065b2f332e71e52e991df4
Poras-oss <55978642+Poras-oss@users.noreply.github.com>
2023-06-07 15:47:45 +0530

    Sending QR data to Firestore done

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -1,23 +1,34 @@
 package com.example.qrscanner;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +36,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         ScanQR();
     }
 
     private void ScanQR(){
+        //Initializing barcode here with some options
         ScanOptions options = new ScanOptions();
         options.setPrompt("Press Volume up to turn on flash");
         options.setBeepEnabled(true);
@@ -38,10 +51,33 @@ public class MainActivity extends AppCompatActivity {
         barLauncher.launch(options);
 
     }
+    private void SendDataToBackEnd(String value){
+        Map<String,Object> data = new HashMap<>();
+        data.put("qrvalue",value);
 
+        firestore.collection(mAuth.getCurrentUser().getUid())
+                .add(data)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                        ScanQR();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        ScanQR();
+                    }
+                });
+    }
+
+    //Barcode Implementation with response handling
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
        if(result.getContents() != null){
-           AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+           SendDataToBackEnd(result.getContents());
+         /*  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
            builder.setTitle("Result");
            builder.setMessage(result.getContents());
            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -50,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                    dialog.dismiss();
                    ScanQR();
                }
-           }).show();
+           }).show();*/
        }
     });
 
